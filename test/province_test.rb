caa8ca8e5dfc6a639be64a5ff661ea8bf9a5582e
Jack Petersen <jack.petersen3@gmail.com>
2022-05-11 18:00:24 -0700

    Extract object instantiation into setup method to run before each test tp avoid duplicate code

--- test/province_test.rb
+++ test/province_test.rb
@@ -3,15 +3,15 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/province'
 require_relative '../lib/data'
-
 class ProvinceTest < Minitest::Test
-
-  def test_province_shortfall
-    asia = Province.new(sample_province_data)
-    assert_equal(5, asia.shortfall)
+  def setup 
+    @asia = Province.new(sample_province_data)
   end
+  def test_province_shortfall
+    assert_equal(5, @asia.shortfall)
+  end
+
   def test_province_profit
-    asia = Province.new(sample_province_data)
-    assert_equal(230, asia.profit)
+    assert_equal(230, @asia.profit)
   end
 end
